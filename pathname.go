91b2ce2cb8c0d48741681149ab8fba15c00bfbb1
sosusy <qwexodn@gmail.com>
2020-05-14 14:26:57 +0900

    Append GetTemplate function

--- pathname.go
+++ pathname.go
@@ -1,12 +1,25 @@
 package gopathname
 
 import (
-	"fmt"
+	"html/template"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+// get file templates
+func GetTemplate(root string) *template.Template {
+	t := template.New(root)
+
+	paths := GetFileNames(root)
+	for _, v := range paths {
+		t, _ = t.ParseFiles(v)
+	}
+
+	return t
+}
+
+// root 폴더 내 전체 파일 이름 가져오기 (경로포함)
 func GetFileNames(root string) []string {
 	var (
 		paths []string
@@ -15,14 +28,10 @@ func GetFileNames(root string) []string {
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			path = strings.ReplaceAll(path, "\\", "/")
-			paths = append(paths, strings.Replace(path, fmt.Sprintf("%v/", root), "", 1))
+			paths = append(paths, path)
 		}
 		return err
 	})
-
-	for _, v := range paths {
-		fmt.Println(v)
-	}
 
 	return paths
 }
--- pathname_test.go
+++ pathname_test.go
@@ -1,11 +1,28 @@
 package gopathname
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
 
 func TestGetFileNames(t *testing.T) {
-	paths := GetFileNames("test")
+	r := gin.Default()
 
-	if len(paths) == 0 {
-		t.Error("None paths")
-	}
+	r.SetHTMLTemplate(GetTemplate("test"))
+
+	r.GET("/", func(c *gin.Context) {
+
+		var query struct {
+			File string `form:"file" binding:"required"`
+		}
+
+		if err := c.Bind(&query); err != nil {
+			t.Error(err)
+		}
+
+		c.HTML(200, query.File, nil)
+	})
+
+	r.Run(":80")
 }
